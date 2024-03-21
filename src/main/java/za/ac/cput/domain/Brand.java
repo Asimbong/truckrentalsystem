eeb20f98dc2de8ddbc7f93466490a24dab7f5cb5
siyanda mthimkhulu <smthimkhulu90@gmail.com>
2024-03-21 16:15:55 +0200

    brand pojo
    brand pojo

---
+++ src/main/java/za/ac/cput/domain/Brand.java
@@ -0,0 +1,95 @@
+package za.ac.cput.domain;
+
+import java.util.Objects;
+/**
+ *Brand.java
+ * @author Siyanda Mthimkhulu (220148279)
+ * Date: 21 March 2024
+ */
+public class Brand {
+
+    private int brandId;
+    private String brandName;
+    private String model;
+    private String color;
+
+    public Brand() {
+    }
+
+    private Brand(Builder builder) {
+        this.brandId = builder.brandId;
+        this.brandName = builder.brandName;
+        this.model = builder.model;
+        this.color = builder.color;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return brandId == brand.brandId && Objects.equals(brandName, brand.brandName) && Objects.equals(model, brand.model) && Objects.equals(color, brand.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brandId, brandName, model, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "brandId=" + brandId +
+                ", brandName='" + brandName + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int brandId;
+        private String brandName;
+        private String model;
+        private String color;
+
+        public Builder setBrandId(int brandId) {
+            this.brandId = brandId;
+            return this;
+        }
+
+        public Builder setBrandName(String brandName) {
+            this.brandName = brandName;
+            return this;
+        }
+
+        public Builder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public Builder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Brand build() {
+            return new Brand(this);
+        }
+    }
+}
