3476b4e6b70cb6c51cb5d5abf22ccba18c0177c6
Asi <221090754@mycput.ac.za>
2024-03-21 11:40:20 +0200

    Changed Driver to Customer and DrverFactory to CustomerFactory

--- src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -1,28 +1,28 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.Driver;
+import za.ac.cput.domain.Customer;
 import za.ac.cput.util.Helper;
 /**
- * DriverFactory.java
+ * CustomerFactory.java
  * This is the factory class
  * @aurthor Asimbonge Mbende (221090754)
  * Date: 20 March 2024
  */
-public class DriverFactory {
-    public static Driver buildDriver(String firstName, String lastName, String email, String idNumber, String licenceNo, String telephoneNo, int rentalID) {
+public class CustomerFactory {
+    public static Customer buildDriver(String firstName, String lastName, String email, String idNumber, String licenceNo, String telephoneNo, int rentalID) {
         if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(email) || Helper.isNullOrEmpty(idNumber)
                 || Helper.isNullOrEmpty(licenceNo) || Helper.isNullOrEmpty(telephoneNo) || Helper.isNullOrEmpty(String.valueOf(rentalID)))
             return null;
-        return new Driver.Builder().setFirstName(firstName).setLastName(lastName)
+        return new Customer.Builder().setFirstName(firstName).setLastName(lastName)
                 .setEmail(email).setIdNumber(idNumber).setLicenceNo(licenceNo)
                 .setTelephoneNo(telephoneNo).setRentalID(rentalID).build();
     }
 
-    public static Driver buildDriver(String firstName, String lastName, String email, int rentalID) {
+    public static Customer buildDriver(String firstName, String lastName, String email, int rentalID) {
         if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(email) || Helper.isNullOrEmpty(String.valueOf(rentalID)))
             return null;
         String idNumber = Helper.generateId();
-        return new Driver.Builder().setIdNumber(idNumber)
+        return new Customer.Builder().setIdNumber(idNumber)
                 .setFirstName(firstName).setLastName(lastName).setEmail(email).setRentalID(rentalID).build();
     }
 
