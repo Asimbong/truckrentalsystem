8f2293cc2d2e7540fffbf37e4e5ed6db4c666afc
siyanda mthimkhulu <smthimkhulu90@gmail.com>
2024-03-21 16:17:58 +0200

    factory class for branch and brand

---
+++ src/main/java/za/ac/cput/BranchFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Branch;
+import za.ac.cput.util.Helper;
+/**
+ *BranchFactory.java
+ * @author Siyanda Mthimkhulu (220148279)
+ * Date: 20 March 2024
+ */
+public class BranchFactory {
+
+    public static Branch createBranch(String branchName, String location, String managerName, int contactNumber) {
+        if (Helper.isNullOrEmpty(branchName) || Helper.isNullOrEmpty(location) || Helper.isNullOrEmpty(managerName)) {
+            return null;
+        }
+
+        return new Branch.Builder()
+                .setBranchName(branchName)
+                .setLocation(location)
+                .setManagerName(managerName)
+                .setContactNumber(contactNumber)
+                .build();
+    }
+}
