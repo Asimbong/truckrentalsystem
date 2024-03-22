c075c77a12418586955e9a3b645fa97097e893ef
Asimbonge Mbende <101199833+Asimbong@users.noreply.github.com>
2024-03-22 13:52:13 +0200

    Update IRepository.java
    Added author name

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,5 +1,10 @@
 package za.ac.cput.repository;
-
+/**
+ * IRepository.java
+ * This is the repository interface
+ * @aurthor Asimbonge Mbende (221090754)
+ * Date: 22 March 2024
+ */
 public interface IRepository <T, ID> {
     T create(T t);
 
