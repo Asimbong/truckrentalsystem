e9de5f9fea02095fa7fa7f347ffdde23b4fb338d
Asimbonge Mbende <101199833+Asimbong@users.noreply.github.com>
2024-03-22 13:51:35 +0200

    Update ICustomerRepository.java
    Added author name

--- src/main/java/za/ac/cput/repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/repository/ICustomerRepository.java
@@ -3,7 +3,12 @@ package za.ac.cput.repository;
 import za.ac.cput.domain.Customer;
 
 import java.util.List;
-
+/**
+ * ICustomerRepository.java
+ * This is the repository interface
+ * @aurthor Asimbonge Mbende (221090754)
+ * Date: 22 March 2024
+ */
 public interface ICustomerRepository extends IRepository<Customer,String> {
     List<Customer>getAll();
 }
