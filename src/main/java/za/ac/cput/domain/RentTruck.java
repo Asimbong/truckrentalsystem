989d5ea5ccadfa139fb769db5e09be63f8dc9732
Thandolwethu Zamasiba Khoza <221797289@mycput.ac.za>
2024-05-03 22:22:17 +0200

    RentTruck_Updated rentTruckFactory rentTruckRepository

--- src/main/java/za/ac/cput/domain/RentTruck.java
+++ src/main/java/za/ac/cput/domain/RentTruck.java
@@ -151,5 +151,6 @@ public class RentTruck {
             return new RentTruck(this);
         }
     }
-    }
+}
+
 
---
+++ src/main/java/za/ac/cput/factory/RentTruckFactory.java
@@ -0,0 +1,34 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.RentTruck;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+
+/**
+ * RentTruckFactory.java
+ * This is the Factory class
+ * @author Thandolwethu Zamasiba Khoza (221797289)
+ * Date: 03 May 2024
+ */
+
+public class RentTruckFactory {
+    public static RentTruck buildRentTruck (int rentId, String branchName, String branchLocation,
+                                            LocalDate rentDate, LocalDate returnDate,
+                                            double totalCost, int customerID) {
+
+        if ((rentId <= 0) ||
+                Helper.isNullOrEmpty(branchName) || Helper.isNullOrEmpty(branchLocation) ||
+                Helper.isNullOrEmpty(String.valueOf(rentDate)) || !Helper.isValidEmail(String.valueOf(returnDate)) ||
+                (totalCost <= 0) || (customerID <= 0))
+
+            return null;
+
+        return new RentTruck.Builder().setRentId(rentId)
+                .setBranchName(branchName)
+                .setRentDate(rentDate)
+                .setReturnDate(returnDate)
+                .setCustomerID(customerID)
+                .build();
+    }
+}
