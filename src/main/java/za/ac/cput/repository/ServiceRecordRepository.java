2fe7d2acbf3b7eaee7c77c66e721f90b2c8ff88a
Asimbonge Mbende <101199833+Asimbong@users.noreply.github.com>
2024-05-04 18:20:38 +0200

    Update ServiceRecordRepository.java
    Corrected date in the author details

--- src/main/java/za/ac/cput/repository/ServiceRecordRepository.java
+++ src/main/java/za/ac/cput/repository/ServiceRecordRepository.java
@@ -5,7 +5,7 @@ import za.ac.cput.domain.ServiceRecord;
 
 /**
  * @aurthor Malesela Modiba (222140275)
- * Date: 23 March 2024
+ * Date: 04 May 2024
  */
 import java.time.LocalDate;
 import java.util.ArrayList;
