05ec88f4e898edc7a97488668579e9037d78fecd
Asi <221090754@mycput.ac.za>
2024-05-18 17:39:40 +0200

    Updated IService and IInsuranceService

--- src/main/java/za/ac/cput/service/IInsuranceService.java
+++ src/main/java/za/ac/cput/service/IInsuranceService.java
@@ -5,5 +5,6 @@ import za.ac.cput.domain.Insurance;
 import java.util.Set;
 
 public interface IInsuranceService extends IService<Insurance,Integer>{
+    Insurance update(Insurance insurance);
     Set<Insurance> getAll();
 }
--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -5,7 +5,5 @@ public interface IService <T, ID> {
 
     T read(ID id);
 
-    T update(T t);
-
     void delete(ID id);
 }
