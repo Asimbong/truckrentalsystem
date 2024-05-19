8cc29eed1cc3e3a9978e8a048ce6928d24fc2e13
MaleselaModiba <222140275@mycput.ac.za>
2024-05-19 21:25:53 +0200

    ServiceRecord & SalesPerson Repository Annotation

--- src/main/java/za/ac/cput/repository/ServiceRecordRepository.java
+++ src/main/java/za/ac/cput/repository/ServiceRecordRepository.java
@@ -1,6 +1,7 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.ServiceRecord;
 
 /**
@@ -10,6 +11,7 @@ import za.ac.cput.domain.ServiceRecord;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+@Repository
 public  interface ServiceRecordRepository extends JpaRepository<ServiceRecord, Integer> {
 
 
